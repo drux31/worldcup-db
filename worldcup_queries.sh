1201671bb43f96d12df504d8e300e5f60449eb74
drux31 <drux31@users.noreply.github.com>
2022-05-26 10:30:12 +0200

    Update two queries to match the expeted result

--- worldcup_queries.sh
+++ worldcup_queries.sh
@@ -17,7 +17,7 @@ echo -e "\nAverage number of goals in all games from the winning teams rounded t
 echo "$($PSQL "SELECT ROUND(AVG(winner_goals),2) FROM games")"
 
 echo -e "\nAverage number of goals in all games from both teams:"
-echo "$($PSQL "SELECT AVG(winner_goals)+AVG(opponent_goals) FROM games")"
+echo "$($PSQL "SELECT round(AVG(winner_goals)+AVG(opponent_goals), 16) FROM games")"
 
 echo -e "\nMost goals scored in a single game by one team:"
 echo "$($PSQL "select max(winner_goals) from games")"
@@ -26,7 +26,7 @@ echo -e "\nNumber of games where the winning team scored more than two goals:"
 echo "$($PSQL "select count(*) from games where winner_goals > 2")"
 
 echo -e "\nWinner of the 2018 tournament team name:"
-echo "$($PSQL "select t.name from teams t left join games g on t.team_id = g.winner_id where g.year = 2018 and round = 'Final'")"
+echo "$($PSQL "select name from teams where team_id in (select winner_id from games where round='Final' and year = 2018) order by name asc")"
 
 echo -e "\nList of teams who played in the 2014 'Eighth-Final' round:"
 echo "$($PSQL "select t.name from teams t left join games g on t.team_id = g.winner_id or t.team_id = g.opponent_id where g.year = 2014 and round = 'Eighth-Final' order by t.name asc")"
